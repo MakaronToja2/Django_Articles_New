93981ae7f8985e86e5fc4f0423bc992d3551cd2b
Makarontoja2 <wloch.jakub2@gmai.com>
2023-05-12 14:05:53 +0200

    first_django_templates

--- article_project/views.py
+++ article_project/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from articles.models import Article
+import random
 
 def home_view(request):
-    name = "Justin"
-    article_obj = Article.objects.get(id=2)
-    HTML_STRING = f"""
-    <h1>{article_obj.title} - Title<br>
-    {article_obj.content} - content</h1>
-    """
+    last_article = Article.objects.last()
+    random_article_id = random.randrange(1,last_article.id+1)
+    article_obj = Article.objects.get(id=random_article_id)
+    content  = {
+        "title": article_obj.title,
+        "content": article_obj.content,
+        "id": article_obj.id,
+
+    }
+    HTML_STRING = render_to_string("home.html", context=content)
     return HttpResponse(HTML_STRING)
