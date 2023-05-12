9947e079e8e8a9b81080a5b83b20598e7d117439
Makarontoja2 <wloch.jakub2@gmai.com>
2023-05-12 13:44:21 +0200

    first_view

--- article_project/views.py
+++ article_project/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 
-
+from articles.models import Article
 
 def home_view(request):
     name = "Justin"
+    article_obj = Article.objects.get(id=2)
     HTML_STRING = f"""
-    <h1>Hello {name}</h1>
+    <h1>{article_obj.title} - Title<br>
+    {article_obj.content} - content</h1>
     """
     return HttpResponse(HTML_STRING)
