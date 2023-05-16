af3f6258d8b8db6b4827292a02fd5c3a06b8e41c
Makarontoja2 <wloch.jakub2@gmai.com>
2023-05-16 18:27:52 +0200

    first_forms

--- articles/views.py
+++ articles/views.py
@@ -10,3 +10,18 @@ def article_detail_view(request, id=None):
         "object": article_obj,
     }
     return render(request, "articles/detail.html", context=context)
+
+def article_search_view(request):
+    query_dict = request.GET
+    query = query_dict.get("query")
+    try:
+        query = int(query_dict.get("query"))
+    except:
+        query = None
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    context = {
+        "object": article_obj,
+    }
+    return render(request, "articles/search.html", context=context)
